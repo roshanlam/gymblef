045d3d7c6c24511f67037a0f0cf392b20730704e
sreesh_p <poudyalsreesh@gmail.com>
2024-02-18 00:28:24 -0500

    did a bunch of stuff

--- lib/login.dart
+++ lib/login.dart
@@ -24,55 +24,63 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Text('Login Page'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Find Your Fit',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset(
+                '../gimbel_assets/top.png',
+                width: MediaQuery.of(context).size.width,
               ),
-            ),
-            SizedBox(height: 32.0),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                fillColor: Colors.black,
-                filled: true,
-                border: OutlineInputBorder(),
+              SizedBox(height: 16.0),
+              Text(
+                'Find Your Fit',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                fillColor: Colors.black,
-                filled: true,
-                border: OutlineInputBorder(),
+              SizedBox(height: 32.0),
+              TextField(
+                controller: emailController,
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(color: Colors.black),
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(),
+                ),
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 32.0),
-            ElevatedButton(
-              onPressed: () {
-                // Add authentication logic here
-                // For now, let's print the entered email and password
-                print('Email: ${emailController.text}');
-                print('Password: ${passwordController.text}');
-              },
-              style: ElevatedButton.styleFrom(
+              SizedBox(height: 16.0),
+              TextField(
+                controller: passwordController,
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(color: Colors.black),
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
               ),
-              child: Text('Login'),
-            ),
-          ],
+              SizedBox(height: 32.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Add authentication logic here
+                  // For now, let's print the entered email and password
+                  print('Email: ${emailController.text}');
+                  print('Password: ${passwordController.text}');
+                },
+                child: Text('Login', style: TextStyle(color: Colors.black)),
+              ),
+            ],
+          ),
         ),
       ),
     );
